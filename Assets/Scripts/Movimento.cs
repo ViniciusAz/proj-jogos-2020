2e527a9bba3ecd00f1017960d9836e9b2e9ed23e
derickpg <garcezderick@gmail.com>
2020-06-17 11:43:38 -0300

    Inclusão de Sprites e Movimentação Personagem

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        anim.SetBool("Frente", false);
+        anim.SetBool("Costas", false);
+        anim.SetBool("Parado", true);
+
+
+
+        if (Input.GetKey("s")) {
+            anim.SetBool("Parado", false);
+            anim.SetBool("Frente", true);
+
+        }
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("Parado", false);
+            anim.SetBool("Costas", true);
+        }
+
+
+    }
+}
