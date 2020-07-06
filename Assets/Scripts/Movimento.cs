b2ea638eed61e5fc14b26014de660b8565015189
ViniciusAz <viniciussazevedo@gmail.com>
2020-07-06 19:36:29 -0300

    movimento e animacao do boneco

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Movimento : MonoBehaviour
 {
-    Animator anim;
+    private Animator anim;
+
+    public float velocidade;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         anim = GetComponent<Animator>();
     }
 
@@ -15,23 +18,29 @@ public class Movimento : MonoBehaviour
     void Update()
     {
 
-        anim.SetBool("Frente", false);
-        anim.SetBool("Costas", false);
-        anim.SetBool("Parado", true);
-
-
-
-        if (Input.GetKey("s")) {
-            anim.SetBool("Parado", false);
-            anim.SetBool("Frente", true);
-
-        }
-        if (Input.GetKey("w"))
-        {
-            anim.SetBool("Parado", false);
+        anim.SetBool("Parado", false);
+        if (Input.GetKey("w") || Input.GetKey("up")) {
             anim.SetBool("Costas", true);
+            transform.position = new UnityEngine.Vector3(transform.position.x, transform.position.y + velocidade);
+            transform.Translate(0, velocidade, 0);
+        } else if (Input.GetKey("a") || Input.GetKey("left")) {
+            anim.SetBool("Esquerda", true);
+            transform.position = new UnityEngine.Vector3(transform.position.x - velocidade, transform.position.y);
+            transform.Translate(-velocidade, 0, 0);
+        } else if (Input.GetKey("s") || Input.GetKey("down")) {
+            anim.SetBool("Frente", true);
+            transform.position = new UnityEngine.Vector3(transform.position.x, transform.position.y - velocidade);
+            transform.Translate(0, -velocidade, 0);
+        } else if (Input.GetKey("d") || Input.GetKey("right")) {
+            anim.SetBool("Direita", true);
+            transform.position = new UnityEngine.Vector3(transform.position.x + velocidade, transform.position.y);
+            transform.Translate(velocidade, 0, 0);
+        } else {
+            anim.SetBool("Parado", true);
+            anim.SetBool("Costas", false);
+            anim.SetBool("Frente", false);
+            anim.SetBool("Esquerda", false);
+            anim.SetBool("Direita", false);
         }
-
-
     }
 }
